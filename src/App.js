8da9c6b4fedc198f95430742ec6f398ef0dc8847
Kayoungkimjs <kykim.dev@gmail.com>
2021-08-02 17:53:07 +0900

    Modify: 초기세팅 수정 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
+class App extends React.Component {
+  render() {
+  return(
+    <div></div>
   );
 }
+};
 
 export default App;
