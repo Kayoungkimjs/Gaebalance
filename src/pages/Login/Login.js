44f22413b72fa335b830994e6d4f902db68a60b3
xia4 <77831217+xia4@users.noreply.github.com>
2021-08-09 18:33:20 +0900

    ADD: 로그인 작업 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { withRouter } from 'react-router';
+// import { withRouter } from 'react-router';
 import './Login.scss';
-
 class Login extends React.Component {
   render() {
-    return <div>가영</div>;
+    return (
+      <main className="container">
+        <div className="contents">
+          <div className="login">
+            <h2>LOG IN</h2>
+            <div className="inp">
+              <input type="text" placeholder="아이디" className="id" />
+              <input type="password" placeholder="비밀번호" className="pw" />
+
+              <div className="check">
+                <input type="checkbox" name="" id="box" />
+                <label>아이디 저장하기</label>
+              </div>
+            </div>
+
+            <button>로그인</button>
+          </div>
+        </div>
+      </main>
+    );
   }
 }
-
 export default Login;
