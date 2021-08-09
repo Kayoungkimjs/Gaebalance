fca1339777c4590e0fa7211393d1a7ecb539ecd2
croissant93 <croissant9393@gmail.com>
2021-08-09 21:04:58 +0900

    Modify: 상세페이지 수정 완료 - 리뷰내용 반영 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
-import Main from './pages/Main/Main';
-import ProductDetail from './pages/ProductDetail/ProductDetail';
+import main from './pages/Main/Main';
+import productDetail from './pages/ProductDetail/ProductDetail';
 
 class Routes extends React.Component {
   render() {
@@ -10,8 +10,8 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
-          <Route exact path="/Main" component={Main} />
-          <Route exact path="/ProductDetail" component={ProductDetail} />
+          <Route exact path="/main" component={main} />
+          <Route exact path="/productDetail" component={productDetail} />
         </Switch>
       </Router>
     );
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,106 +1,109 @@
 import React from 'react';
 import '../ProductDetail/ProductDetail.scss';
+import OrderListBox from './OrderListBox';
+import ProductInfoBox from './ProductInfoBox';
+import SizePicker from './SizePicker';
 
 class ProductDetail extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      imgList: [
-        {
-          src: '/images/ProductDetail/mockProduct.png',
-        },
-        {
-          src: '/images/ProductDetail/mockProduct.jpg',
-        },
-        {
-          src: '/images/ProductDetail/mockProduct.jpg',
-        },
-        {
-          src: '/images/ProductDetail/mockProduct.jpg',
-        },
-        {
-          src: '/images/ProductDetail/mockProduct.jpg',
-        },
-      ],
+      productInfoList: {},
+      mainImage: '',
+      selected: [],
     };
   }
 
+  componentDidMount() {
+    fetch('http://10.58.0.58:8000/products/3', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(result => {
+        this.setState({ productInfoList: result, mainImage: result.image_url });
+      });
+  }
+
   imgChange = e => {
-    console.log(e.target);
+    this.setState({
+      mainImage: e.target.src,
+    });
+  };
+
+  addSelected = value => {
+    this.setState({
+      selected: this.state.selected.concat({ size: value }),
+    });
   };
 
   render() {
-    console.log(this.state.imgList);
     return (
       <article className="productDetail">
         <div className="container">
           <section className="ProductDetailTop">
             <article className="imageBox">
               <div className="mainImage">
-                <img src="/images/ProductDetail/mockProduct.png" alt="" />
+                <img src={this.state.mainImage} alt="mainImage" />
               </div>
               <div className="imageExpandBtn">
-                <i class="fas fa-expand"></i>
-                <span>EXPAND VIEW</span>
+                <i className="fas fa-expand"></i>
+                <div className="expandBtn">EXPAND VIEW</div>
               </div>
               <div className="imageList">
-                {this.state.imgList.map((item, index) => {
+                {this.state.productInfoList.image_list?.map((item, index) => {
                   return (
-                    <div>
+                    <div key={index} className="imageListBox">
                       <img
-                        src={item.src}
-                        alt="두번째 이미지"
-                        key={index}
+                        src={item}
+                        alt="신발 이미지"
                         onClick={this.imgChange}
                       />
                     </div>
                   );
                 })}
-                {/* <div>
-                <img src="" alt="첫번째 이미지" />
-              </div>
-              <div>
-                <img
-                  src="/images/ProductDetail/mockProduct.jpg"
-                  alt="두번째 이미지"
-                />
-              </div>
-              <div>
-                <img src="" alt="세번째 이미지" />
-              </div>
-              <div>
-                <img src="" alt="네번째 이미지" />
-              </div>
-              <div>
-                <img src="" alt="닷번째 이미지" />
-              </div> */}
               </div>
             </article>
             <article className="productOption">
               <div className="productHeader">
-                <div className="productName">프레쉬폼x 1080 V11 (4E)</div>
+                <div className="productName">
+                  {this.state.productInfoList.name}
+                </div>
                 <div className="productTag">
                   <img src="/images/ProductDetail/tag-best.png" alt="" />
                   <img src="/images/ProductDetail/tag-new.png" alt="" />
                 </div>
                 <div className="ProductRating">
                   <div className="starPoint">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                   </div>
                   <div className="ratingBox">
-                    <span className="currentRating">4.7</span> / 5
-                    <span className="reviewCount"> (17)</span>
+                    <div>
+                      <span className="currentRating">4.7</span> / 5
+                      <span className="reviewCount"> (17)</span>
+                    </div>
+
+                    <div className="etcBtnBox">
+                      <div className="heartBox">
+                        <i className="far fa-heart"></i>
+                      </div>
+                      <i className="fab fa-facebook-f"></i>
+                      <i className="fas fa-dove"></i>
+                    </div>
                   </div>
                 </div>
-
                 <div className="priceBox">
                   <div>
-                    <span className="productPrice">159,000원</span>
+                    <span className="productPrice">
+                      {Math.floor(
+                        this.state.productInfoList.price
+                      ).toLocaleString('ko-KR')}
+                      <span className="productWon">원</span>
+                    </span>
                     <span className="productSavePoint">적립 9,540(6%)</span>
                   </div>
                 </div>
@@ -110,31 +113,10 @@ class ProductDetail extends React.Component {
                   <div className="colorPickerTitle">색상</div>
                   <div className="colorPicker">
                     <div>
-                      <i class="fas fa-circle fa-2x cWhite"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cGray"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cBlack"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cRed"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cOrange"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cYellow"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cGreen"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cWhite"></i>
-                    </div>
-                    <div>
-                      <i class="fas fa-circle fa-2x cWhite"></i>
+                      <i
+                        className="fas fa-circle fa-2x cWhite"
+                        style={{ color: this.state.productInfoList.color }}
+                      ></i>
                     </div>
 
                     {/* 생상 선택 추가 예정 */}
@@ -143,24 +125,39 @@ class ProductDetail extends React.Component {
                 <div className="sizePickerBox">
                   <div className="sizePickerTitle">사이즈</div>
                   <div className="sizePicker">
-                    <button>S</button>
-                    <button>M</button>
-                    <button>L</button>
+                    <SizePicker
+                      sizeName="S"
+                      class="sizeS"
+                      addSelected={this.addSelected}
+                    />
+                    <SizePicker
+                      sizeName="M"
+                      class="sizeM"
+                      addSelected={this.addSelected}
+                    />
+                    <SizePicker
+                      sizeName="L"
+                      class="sizeL"
+                      addSelected={this.addSelected}
+                    />
                   </div>
                 </div>
               </div>
               <div className="orderListBox">
-                <div className="orderList">
-                  <div className="orderName">(30)Yellow/275/4E</div>
-                  <div className="orderQuantity">수량 선택</div>
-                  <div className="orderPrice">159,000 원</div>
-                  <div>
-                    <i class="fas fa-times"></i>
-                  </div>
-                </div>
+                {this.state.selected.map((item, index) => {
+                  return (
+                    <OrderListBox
+                      orderList={item}
+                      productInfo={this.state.productInfoList}
+                      size={this}
+                      key={index}
+                    />
+                  );
+                })}
+
                 <div className="totalPriceBox">
                   <div className="totalPriceName">합계</div>
-                  <div className="totalPrice">159,000 원</div>
+                  <div className="totalPrice">0 원</div>
                 </div>
                 <div>
                   <div></div>
@@ -170,16 +167,21 @@ class ProductDetail extends React.Component {
               <div className="purchaseBox">
                 <button className="basketBtn">장바구니</button>
                 <button className="purchaseBtn">구매하기</button>
+                <div>
+                  * 주문/배송/반품 등 일반 문의는 1:1 문의를 이용해 주시기
+                  바랍니다.
+                </div>
+              </div>
+              <div className="productNotifyBox">
+                <div className="productNotify">원하는 옵션이 없으신가요?</div>
+                <button className="productNotifyBtn">
+                  <i className="far fa-bell "></i>입고 알림 신청하기
+                </button>
               </div>
             </article>
           </section>
-          <section className="ProductDetailBottom">
-            <div className="ProductDetailNav"></div>
-            <div>
-              <div></div>
-              <div></div>
-            </div>
-          </section>
+          <ProductInfoBox info={this.state.productInfoList} />
+          <section></section>
         </div>
       </article>
     );
