ff63a6ef98f48c34c9fc67f3750dd921da5fbca2
xia4 <77831217+xia4@users.noreply.github.com>
2021-08-10 19:14:19 +0900

    Add:Nav 피드백 수정 완료료

--- src/components/Nav/Nav/Category.js
+++ src/components/Nav/Nav/Category.js
@@ -128,6 +128,136 @@ class Category extends React.Component {
               </li>
             </ul>
           </li>
+          <li>
+            <a href="#">KID</a>
+            <ul>
+              <li>
+                <a href="#">상의</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                  <li>
+                    <a href="#">긴팔</a>
+                  </li>
+                  <li>
+                    <a href="#">반팔</a>
+                  </li>
+                  <li>
+                    <a href="#">트레이닝</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">하의</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                  <li>
+                    <a href="#">긴바지</a>
+                  </li>
+                  <li>
+                    <a href="#">반바지</a>
+                  </li>
+                  <li>
+                    <a href="#">트레이닝</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">아우터</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                  <li>
+                    <a href="#">자켓</a>
+                  </li>
+                  <li>
+                    <a href="#">패딩</a>
+                  </li>
+                  <li>
+                    <a href="#">가디건</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">비치웨어</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">
+              <span>GB&YOU</span>
+            </a>
+            <ul>
+              <li>
+                <a href="#">상의</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                  <li>
+                    <a href="#">긴팔</a>
+                  </li>
+                  <li>
+                    <a href="#">반팔</a>
+                  </li>
+                  <li>
+                    <a href="#">트레이닝</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">하의</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                  <li>
+                    <a href="#">긴바지</a>
+                  </li>
+                  <li>
+                    <a href="#">반바지</a>
+                  </li>
+                  <li>
+                    <a href="#">트레이닝</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">아우터</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                  <li>
+                    <a href="#">자켓</a>
+                  </li>
+                  <li>
+                    <a href="#">패딩</a>
+                  </li>
+                  <li>
+                    <a href="#">가디건</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">비치웨어</a>
+                <ul>
+                  <li>
+                    <a href="#">전체보기</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
         </ul>
       </nav>
     );
