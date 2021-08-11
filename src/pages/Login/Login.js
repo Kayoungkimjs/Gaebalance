0f253054e66fb75aa682551ed0d737f4caa12ebc
xia4 <77831217+xia4@users.noreply.github.com>
2021-08-11 16:56:33 +0900

    Add:로그인 API 연결 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,35 @@ import React from 'react';
 // import { withRouter } from 'react-router';
 import './Login.scss';
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      id: '',
+      pw: '',
+    };
+  }
+  goToMain = () => {
+    const { id, pw } = this.state;
+
+    if (id.includes('@') && pw.length > 4) {
+      fetch('http://10.58.5.112:8000/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email: id, password: pw }),
+      })
+        .then(response => response.json())
+        .then(result => {
+          this.props.history.push('./Main');
+        });
+    }
+  };
+
+  handleIdInput = event => {
+    this.setState({ id: event.target.value });
+  };
+  handlePwInput = event => {
+    this.setState({ pw: event.target.value });
+  };
+
   render() {
     return (
       <main className="loginContainer">
@@ -9,8 +38,18 @@ class Login extends React.Component {
           <div className="login">
             <h2>LOG IN</h2>
             <div className="inp">
-              <input type="text" placeholder="아이디" className="id" />
-              <input type="password" placeholder="비밀번호" className="pw" />
+              <input
+                type="text"
+                placeholder="아이디"
+                className="id"
+                onChange={this.handleIdInput}
+              />
+              <input
+                type="password"
+                placeholder="비밀번호"
+                className="pw"
+                onChange={this.handlePwInput}
+              />
 
               <div className="check">
                 <input type="checkbox" name="" id="box" />
@@ -18,7 +57,18 @@ class Login extends React.Component {
               </div>
             </div>
 
-            <button>로그인</button>
+            <button
+              className={
+                this.state.id.includes('@') && this.state.pw.length > 4
+                  ? 'login-btn-active'
+                  : 'login-btn'
+              }
+              onClick={this.goToMain}
+              name="button"
+              type="button"
+            >
+              로그인
+            </button>
           </div>
         </div>
       </main>
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router';
-import './Main.scss';
+// import './Main.scss';
 
 class Main extends React.Component {
   render() {
