337b522f347bce8c779cba1217a305d55929d6ab
xia4 <77831217+xia4@users.noreply.github.com>
2021-08-12 17:12:35 +0900

    Add: 비구조할당 적용, 주석삭제

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'http://10.58.0.58:8000';
-const ASE_URL = 'http://10.58.5.112:8000';
+const BASE_URL_2 = 'http://10.58.5.112:8000';
 export const API = {
   PRODUCT: `${BASE_URL}/products`,
-  JOIN: `${ASE_URL}/users/join`,
+  JOIN: `${BASE_URL_2}/users/join`,
 };
--- src/pages/Checkbox/Checkbox.js
+++ src/pages/Checkbox/Checkbox.js
@@ -2,19 +2,6 @@ import React from 'react';
 import { ReactDOM } from 'react';
 import '../Checkbox/Checkbox.scss';
 
-const colors = [
-  '빨강',
-  '주황',
-  '노랑',
-  '초록',
-  '파랑',
-  '남색',
-  '보라',
-  '검정',
-  '흰색',
-  '회색',
-];
-
 class Checkbox extends React.Component {
   constructor() {
     super();
@@ -37,31 +24,21 @@ class Checkbox extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange(event) {
-    var isChecked = event.target.checked;
-    var item = event.target.value;
+
+  handleChange() {
+    const { checked, value } = this.state;
 
     this.setState(prevState => ({
-      checkedItems: prevState.checkedItems.set(item, isChecked),
+      checkedItems: prevState.checkedItems.set(checked, value),
     }));
   }
 
   handleSubmit(event) {
-    console.log(this.state);
     event.preventDefault();
   }
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { isCheckOn: true };
-  // }
-
-  // handleClick = () => {
-  //   this.setState(state => ({
-  //     isCheckOn: !state.isCheckOn,
-  //   }));
-  // };
 
   render() {
+    const { categories } = this.state;
     return (
       <div className="row">
         <label className="ftit">
@@ -70,11 +47,11 @@ class Checkbox extends React.Component {
         </label>
         <div className="fdata">
           <form className="chk" onSubmit={this.handleSubmit}>
-            {this.state.categories.map(item => (
-              <li className="listr">
+            {categories.map(item => (
+              <li className="chkList">
                 <label>
                   <input
-                    className="ip_chekbox"
+                    className="ipChekbox"
                     type="checkbox"
                     value={item.id}
                     onChange={this.handleChange}
@@ -85,109 +62,6 @@ class Checkbox extends React.Component {
             ))}
           </form>
         </div>
-        {/* <div className="fdata">
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>빨강</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>주황</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>노랑</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>초록</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>파랑</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>남색</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>보라</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>검정</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>흰색</label>
-          </span>
-          <span className="chk">
-            <input
-              type="checkbox"
-              name="color"
-              className="ip_chekbox"
-              value="0"
-              onClick={this.handleClick}
-            />
-            <label>회색</label>
-          </span>
-          <em className="ip_info">최대 3개 선택 가능</em>
-        </div> */}
       </div>
     );
   }
