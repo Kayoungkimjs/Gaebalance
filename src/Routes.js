fd3fe452c83cdd9107f0f0db8681388cc6bec6ec
croissant93 <croissant9393@gmail.com>
2021-08-12 20:28:59 +0900

    Add: 장바구니 페이지 완료 - 장바구니 레이아웃 구현완료 - 장바구니 기능 구현완료

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import main from './pages/Main/Main';
 import productDetail from './pages/ProductDetail/ProductDetail';
+import shoppingCart from './pages/ShoppingCart/ShoppingCart';
 
 class Routes extends React.Component {
   render() {
@@ -11,7 +12,8 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/main" component={main} />
-          <Route exact path="/productDetail" component={productDetail} />
+          <Route exact path="/productDetail/:id" component={productDetail} />
+          <Route exact path="/shoppingCart" component={shoppingCart} />
         </Switch>
       </Router>
     );
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'http://10.58.5.89:8000';
+export const API = {
+  SERVER: `${BASE_URL}`,
+  PRODUCT: `${BASE_URL}/products`,
+};
